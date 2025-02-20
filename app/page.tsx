c0b8ff14d186c1fc8dbce1cf3fd8149f7ad4b618
Akira37 <863781751@qq.com>
2025-02-20 21:03:39 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import React from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello Akira!!! Hyperplasma!!!!!</h1>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Hello Akira!!! Hyperplasma!!!!!</h1>
+            {React.createElement("h1", null, "Hyplus Gooooooooooooo~~~~~~~~~")}
+        </main>
+    )
 }
