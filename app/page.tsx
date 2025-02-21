40c998d8cf420e8ad60a33bdca829265c5fa19b2
Akira37 <863781751@qq.com>
2025-02-21 16:24:05 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Hello Akira!!! Hyperplasma!!!!!</h1>
+            <h1>KINA</h1>
             {React.createElement("h1", null, "Hyplus Gooooooooooooo~~~~~~~~~")}
         </main>
     )
