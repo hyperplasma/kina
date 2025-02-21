b99f407e5afedd92590ca814cb8cf1c7fc82be71
Akira37 <863781751@qq.com>
2025-02-21 20:31:21 +0800

    Update

--- components/home/Navigation/Toolbar.tsx
+++ components/home/Navigation/Toolbar.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import {useAppContext} from "@/components/AppContext";
-import {MdDarkMode, MdInfo, MdLightMode} from "react-icons/md";
+import {MdDarkMode, MdHome, MdInfo, MdLightMode, MdLink} from "react-icons/md";
 import {ActionType} from "@/reducers/AppReducer";
 
 export default function Toolbar() {
@@ -12,7 +12,7 @@ export default function Toolbar() {
         <div className="absolute bottom-0 left-0 right-0 bg-gray-800 flex p-2 justify-between">
             <Button
                 icon={themeMode === "dark" ? MdDarkMode : MdLightMode}
-                variant="text"
+                variant="outline"
                 onClick={() => {
                     dispatch({
                         type: ActionType.UPDATE,
@@ -20,10 +20,18 @@ export default function Toolbar() {
                         value: themeMode === "dark" ? "light" : "dark"
                     })
                 }}
+                title="Return to Hyperplasma"
+            />
+            <Button
+                icon={MdHome}
+                variant="outline"
+                onClick={() => {
+                    window.open("https://www.hyperplasma.top/hyplus", "_blank")
+                }}
             />
             <Button
                 icon={MdInfo}
-                variant="text"
+                variant="outline"
             />
         </div>
     )
